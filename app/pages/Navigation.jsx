316ba3dff837206125cff898fdd89ef1dfb1c519
clio001 <johnwoitkowitz@hotmail.com>
2023-09-14 20:36:08 +0200

    added links to logos and about

--- app/pages/Navigation.jsx
+++ app/pages/Navigation.jsx
@@ -1,24 +1,34 @@
+"use client";
 import { Box, Typography } from "@mui/material";
 import React from "react";
 import navigation from "../styles/navigation.module.css";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 function Navigation() {
+  const router = useRouter();
   return (
     <div>
       <Box className={navigation.navbar_container}>
         <Box>
-          <img
-            src="/stabi-logo-kante-weiss.png"
-            className={navigation.navbar_logo}
-          />
-          <img
-            src="/IN_CONTEXT_Schriftzug ohne Rand weiss.png"
-            className={navigation.navbar_logo}
-          />
+          <Link href="http://www.staatsbibliothek-berlin.de" target="_blank">
+            <img
+              src="/stabi-logo-kante-weiss.png"
+              className={navigation.navbar_logo}
+            />
+          </Link>
+          <Link href="/">
+            <img
+              src="/IN_CONTEXT_Schriftzug ohne Rand weiss.png"
+              className={navigation.navbar_logo}
+            />
+          </Link>
         </Box>
         <Box className={navigation.navbar_items}>
           {" "}
-          <Typography>About</Typography>
+          <Link href="/about">
+            <Typography>About</Typography>
+          </Link>
           <Typography>My Workplace</Typography>
           <Typography>Documentation</Typography>
           <Typography>EN | DE</Typography>
