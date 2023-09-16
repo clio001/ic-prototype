37b7b086faacf5a8488ca8385cde0027c06174bf
clio001 <johnwoitkowitz@hotmail.com>
2023-09-16 15:47:25 +0200

    created page.jsx

---
+++ app/listview/[id]/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ItemView() {
+  return <div>ItemView</div>;
+}
+
+export default ItemView;
---
+++ app/myworkspace/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyWorkspace() {
+  return <div>MyWorkspace</div>;
+}
+
+export default MyWorkspace;
