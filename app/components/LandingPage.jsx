f77cb83afbf3ccf0f0839987c6823eca557af9b7
clio001 <johnwoitkowitz@hotmail.com>
2023-09-16 17:58:36 +0200

    added feature modules

--- app/components/LandingPage.jsx
+++ app/components/LandingPage.jsx
@@ -1,7 +1,20 @@
-import { Box, Button, TextField } from "@mui/material";
+"use client";
+import { Box, IconButton, Paper, TextField, Typography } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { useRouter } from "next/navigation";
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 function LandingPage() {
+  const router = useRouter();
+
+  const SearchButton = () => (
+    <IconButton onClick={() => router.push("/listview")}>
+      <SearchIcon />
+    </IconButton>
+  );
+
   return (
     <>
       <Box className="search_field_container">
@@ -11,12 +24,47 @@ function LandingPage() {
           label="Search collections ..."
           variant="outlined"
           InputProps={{
-            autoComplete: "off", // Disable autocomplete
+            endAdornment: <SearchButton />,
           }}
         />
-        <Button variant="contained" color="primary" size="large">
-          Find
-        </Button>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <Paper
+          className="feature1_image"
+          elevation={5}
+          sx={{
+            backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/1/1d/Heinrich_Barth%27s_route_through_Africa%2C_1850_to_1855.jpg")`,
+          }}
+        ></Paper>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            marginLeft: "2rem",
+          }}
+        >
+          <Paper
+            elevation={5}
+            className="feature2_image"
+            sx={{
+              backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/R%C3%BChlmannorgel_Herrnhut.jpg/320px-R%C3%BChlmannorgel_Herrnhut.jpg")`,
+            }}
+          ></Paper>
+          <Paper
+            elevation={5}
+            className="feature2_image"
+            sx={{
+              backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Kaiser-Wilhelms-Land-Brockhaus_b10_nr0030a_hfid_5181872.jpg/1920px-Kaiser-Wilhelms-Land-Brockhaus_b10_nr0030a_hfid_5181872.jpg")`,
+            }}
+          ></Paper>
+        </Box>
       </Box>
     </>
   );
