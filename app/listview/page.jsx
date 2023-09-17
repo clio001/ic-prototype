c4a0f650af8097b046715b8bf721b73afbe6fb1b
clio001 <johnwoitkowitz@hotmail.com>
2023-09-17 13:55:25 +0200

    added listitem to listview

--- app/listview/page.jsx
+++ app/listview/page.jsx
@@ -1,7 +1,19 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
+import ListItem from "../components/ListItem";
+import listview from "../styles/listview.module.css";
 
 function ListView() {
-  return <div>ListView</div>;
+  return (
+    <Box className={listview.page_container}>
+      <Typography variant="h4" sx={{ padding: "3rem" }}>
+        Results
+      </Typography>
+      <Box className={listview.result_container}>
+        <ListItem />
+      </Box>
+    </Box>
+  );
 }
 
 export default ListView;
